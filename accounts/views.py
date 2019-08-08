5efc1aa62522ba1f7fca7c6b5b0c9ff5f53cd444
christianvilla <christianj.villa@gmail.com>
2019-08-07 17:14:06 -0700

    adds devbook htmls

--- accounts/views.py
+++ accounts/views.py
@@ -9,22 +9,18 @@ def register(request):
         # get form information
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
-        username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
+        birthday = request.POST['birthday']
         # check if the passwords match
         if password == password2:
-        # check if user exists with that username
-            if User.objects.filter(username=username).exists():
-                return render(request, 'register.html', {'error': 'Username is already taken.'})
         # check if user exists with that email
-            else:
-                if User.objects.filter(email=email).exists():
+            if User.objects.filter(email=email).exists():
                     return render(request, 'register.html', {'error': 'Email already exists. Please Log in.'})
         # if everything is okay Create a User
-                else:
-                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)   
+            else:
+                    user = User.objects.create_user(password=password, email=email, first_name=first_name, last_name=last_name, birthdat=birthday)   
                     user.save()
                     return redirect('login')
         else:
@@ -37,11 +33,11 @@ def register(request):
 def login(request):
     # if POST 
     if request.method == 'POST':
-        username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
         # GET form info
         # authenticate user
-        user = auth.authenticate(username=username, password=password)
+        user = auth.authenticate(email=email, password=password)
         # make sure a user exists
         if user is not None:
             # login
@@ -58,4 +54,4 @@ def login(request):
     
 def logout(request):
     auth.logout(request);
-    return redirect('artist_list')
+    return redirect('signup')
