1a40d92d9facb4f284ee42eb6fb53f635f8a5279
christianvilla <christianj.villa@gmail.com>
2019-08-08 14:40:22 -0700

    adds profile image and url to profile

--- devbook/admin.py
+++ devbook/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Post, Comment, Like
+from .models import Post, Comment, Like, Profile
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Like)
+admin.site.register(Profile)
+
