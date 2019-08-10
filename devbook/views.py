3cc91a1a2f4c3479c633714f7b6a24cbb1133d12
Lawrence <lawrencema415@gmail.com>
2019-08-10 16:02:11 -0700

    added specific user profile page can be accessed through clicking icon

--- devbook/views.py
+++ devbook/views.py
@@ -91,7 +91,7 @@ def post_comment(request,pk):
     else:
         form = CommentForm()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-    
+
 def comment_edit(request, pk):
     comment = Comment.objects.get(id=pk)
     if request.method == 'POST':
@@ -122,6 +122,6 @@ def like_post(request,pk):
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def user_prof(request,pk):
-    profile = profile.objects.get(id=pk)
+    profile = Profile.objects.get(id=pk)
     profile_form = ProfileForm()
-    return redirect(request, 'userprofile.html',{"profile":profile,"profile_form":profile_form })
+    return render(request, 'userprofile.html',{"profile":profile,"profile_form":profile_form })
