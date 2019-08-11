106dbcd473d016a34a910a3f3bdc7ad2f536d96c
christianvilla <christianj.villa@gmail.com>
2019-08-11 11:45:40 -0700

    adds setting for email

--- devbook_project/settings.py
+++ devbook_project/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #During development only
 
 # Application definition
 
--- devbook_project/urls.py
+++ devbook_project/urls.py
@@ -19,8 +19,9 @@ from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('devbook.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
