a05d734d79c45c226671e8b1e4f52b1dafcdcaf5
christianvilla <christianj.villa@gmail.com>
2019-08-11 12:54:42 -0700

    adds functional email sent on register

--- devbook_project/settings.py
+++ devbook_project/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'devbookdevs@gmail.com'
+EMAIL_HOST_PASSWORD = 'devbook123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
